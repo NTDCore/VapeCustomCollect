c8c59aab1cce6f89288262b28ebfaae4560f8a28
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-08-27 19:02:47 +0700

    update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,1 +1,34 @@
+chattagloaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScriptlua", true))
+local function chattagloaded()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/tag/main/load.lua"))
+end
+local function sape()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/Initiate.lua"))()
+end
+local function cat()
+	loadstring(readfile("vape/vape/CustomModules/6872274481.lua"))()
+end
+local function mycustom()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/VapeCustomCollect/main/CustomModules/bedwars.lua", true))()
+end
+if shared.triocustom then
+	mycustom()
+	cat()
+	sape()
+end
 
+if shared.sapewithntdcore then
+sape()
+mycustom()
+end
+
+if shared.sapewithcat then
+		sape()
+		cat()
+end
+
+if shared.catwithntdcore then
+	cat()
+	mycustom()
+end
